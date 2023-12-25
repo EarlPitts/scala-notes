088fbb7da685e46654f0c0667e7b2ff42380704c
Bendeguz Seres <grafitpor@gmail.com>
2023-12-25 10:26:19 +0100

    Implement State monad

--- src/main/scala/FuncState.scala
+++ src/main/scala/FuncState.scala
@@ -16,7 +16,7 @@ trait RNG:
 // and showing that it's an instance of RNG at once
 case class SimpleRNG(seed: Long) extends RNG:
   def nextInt(): (Int, RNG) =
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val newSeed = (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
@@ -28,38 +28,38 @@ val (n1, rng3) = rng2.nextInt()
 def nonNegativeInt(rng: RNG): (Int, RNG) =
   val (n, nextRng) = rng.nextInt()
   if n >= 0 && n <= Int.MaxValue && n != Int.MinValue
-    then (n, nextRng)
-    else nonNegativeInt(nextRng)
+  then (n, nextRng)
+  else nonNegativeInt(nextRng)
 
 def double(rng: RNG): (Double, RNG) =
   val (n, nextRng) = rng.nextInt()
   if n > 1
-    then (1.0 / n.toDouble, nextRng)
-    else double(nextRng)
+  then (1.0 / n.toDouble, nextRng)
+  else double(nextRng)
 
 def intDouble(rng: RNG): ((Int, Double), RNG) =
   val (n1, rng2) = nonNegativeInt(rng)
   val (n2, rng3) = double(rng2)
-  ((n1,n2),rng3)
+  ((n1, n2), rng3)
 
-def doubleInt(rng: RNG): ((Double,Int), RNG) =
+def doubleInt(rng: RNG): ((Double, Int), RNG) =
   val (n1, rng2) = double(rng)
   val (n2, rng3) = nonNegativeInt(rng2)
-  ((n1,n2),rng3)
+  ((n1, n2), rng3)
 
-def double3(rng: RNG): ((Double,Double,Double), RNG) =
+def double3(rng: RNG): ((Double, Double, Double), RNG) =
   val (n1, rng2) = double(rng)
   val (n2, rng3) = double(rng2)
   val (n3, rng4) = double(rng3)
-  ((n1,n2,n3),rng4)
+  ((n1, n2, n3), rng4)
 
 def ints(count: Int)(rng: RNG): (List[Int], RNG) =
   if count == 0
-    then (List(),rng)
-    else
-      val (n: Int,rng2: RNG) = rng.nextInt()
-      val (is: List[Int],rng3: RNG) = ints(count-1)(rng2)
-      (n :: is, rng3)
+  then (List(), rng)
+  else
+    val (n: Int, rng2: RNG) = rng.nextInt()
+    val (is: List[Int], rng3: RNG) = ints(count - 1)(rng2)
+    (n :: is, rng3)
 
 // RNG State Action
 type Rand[+A] = RNG => (A, RNG)
@@ -69,7 +69,7 @@ val int: Rand[Int] = rng => rng.nextInt()
 def unit[A](a: A): Rand[A] =
   rng => (a, rng)
 
-def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
   rng =>
     val (a, rng2) = s(rng)
     (f(a), rng2)
@@ -80,13 +80,13 @@ def nonNegativeEven: Rand[Int] =
 def betterDouble(rng: RNG): (Double, RNG) =
   map(int)(n => scala.math.abs(1.0 / (n + 2)))(rng)
 
-def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
+def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
   rng =>
     val (a, rng2) = ra(rng)
     val (b, rng3) = rb(rng2)
-    (f(a,b), rng3)
+    (f(a, b), rng3)
 
-def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
   map2(ra, rb)((_, _))
 
 val randIntDouble: Rand[(Int, Double)] =
@@ -108,27 +108,72 @@ def nonNegativeLessThan(n: Int): Rand[Int] =
   rng =>
     val (i, rng2) = nonNegativeInt(rng)
     val mod = i % n
-    if i + (n-1) - mod >= 0
-      then (mod, rng2)
-      else nonNegativeLessThan(n)(rng2)
+    if i + (n - 1) - mod >= 0
+    then (mod, rng2)
+    else nonNegativeLessThan(n)(rng2)
 
-def flatMap[A,B](r: Rand[A])(f: A => Rand[B]): Rand[B] =
+def flatMap[A, B](r: Rand[A])(f: A => Rand[B]): Rand[B] =
   rng0 =>
-    val (a,rng1) = r(rng0)
+    val (a, rng1) = r(rng0)
     f(a)(rng1)
 
 def nonNegLessThan(n: Int): Rand[Int] =
   flatMap(nonNegativeInt): i =>
     val mod = i % n
-    if i + (n-1) - mod >= 0
-      then unit(mod)
-      else nonNegLessThan(n)
+    if i + (n - 1) - mod >= 0
+    then unit(mod)
+    else nonNegLessThan(n)
 
-def mapFlatMap[A,B](r: Rand[A])(f: A => B): Rand[B] =
+def mapFlatMap[A, B](r: Rand[A])(f: A => B): Rand[B] =
   flatMap(r): a =>
     unit(f(a))
 
-def map2FlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
+def map2FlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
   flatMap(ra): a =>
     flatMap(rb): b =>
-      unit(f(a,b))
+      unit(f(a, b))
+
+def rollDie2: Rand[Int] = map(nonNegativeLessThan(6))(_ + 1)
+
+def roll3: Rand[(Int, Int, Int)] =
+  flatMap(rollDie2): d1 =>
+    flatMap(rollDie2): d2 =>
+      flatMap(rollDie2): d3 =>
+        unit((d1, d2, d3))
+
+// General State type
+// type State[S, +A] = S => (A, S)
+
+final case class State[S, +A](runState: S => (A, S))
+
+// type Rand[A] = State[RNG, A]
+
+object State:
+  def pure[S,A](a: A): State[S,A] =
+    State(s => (a,s))
+  
+  def map[S, A, B](sa: State[S, A])(f: A => B): State[S, B] =
+    State((s: S) =>
+      val (v, s1) = sa.runState(s)
+      (f(v), s)
+    )
+
+  def bind[S, A, B](sa: State[S, A])(f: A => State[S,B]): State[S, B] =
+    State((s1: S) =>
+        val (a, s2) = sa.runState(s1)
+        f(a).runState(s2))
+
+  def execState[S,A](sa: State[S,A]): S => S =
+    (s: S) =>
+      sa.runState(s)._2
+
+  def evalState[S,A](sa: State[S,A]): S => A =
+    (s: S) =>
+      sa.runState(s)._1
+
+  def get[S]: State[S,S] = State(s => (s,s))
+
+  def put[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] =
+    bind(get)(s => put(f(s)))
