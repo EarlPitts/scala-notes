625830ba18b52a14965c7f2eeeef04c2ea4f315b
Bendeguz Seres <grafitpor@gmail.com>
2024-01-25 08:01:11 +0100

    ExecutionContext

--- src/main/scala/effects/CatIO.scala
+++ src/main/scala/effects/CatIO.scala
@@ -8,6 +8,7 @@ import scala.concurrent._
 import scala.io.Source
 import cats.syntax.all._
 import cats.effect.kernel.Deferred
+import java.util.concurrent.Executors
 
 object Main extends App:
   val hw: IO[Unit] = IO.delay(println("Hello world!"))
@@ -471,3 +472,52 @@ object Timeout extends IOApp.Simple:
         IO.sleep(duration) *>
         IO(s"$name: done").myDebug
     ).onCancel(IO(s"$name: cancelled").myDebug.void).void
+
+object Parallelism extends IOApp.Simple:
+  import debug.*
+
+  def run: IO[Unit] = for
+    _ <- IO(s"number of CPSs: $numCpus").myDebug
+    _ <- tasks.myDebug
+  yield ()
+
+  val numCpus = Runtime.getRuntime().availableProcessors()
+  val tasks = List.range(0, numCpus * 2).parTraverse(task)
+  def task(i: Int): IO[Int] = IO(i).myDebug
+
+object Blocking extends IOApp.Simple:
+  import debug.*
+
+  def tickingClock: IO[Unit] = for
+    _ <- IO(System.currentTimeMillis()).myDebug
+    _ <- IO.sleep(1.second)
+    // _ <- IO.cede // Unnecessary, every bind is an async boundary
+    _ <- tickingClock
+  yield ()
+
+  def run: IO[Unit] = for
+    // _ <- IO("on default threadpool").myDebug
+    // _ <- IO.blocking("on blocking threadpool").myDebug
+    _ <- (tickingClock, tickingClock, tickingClock, tickingClock).parTupled
+  yield ()
+
+object Shifting extends IOApp.Simple:
+  import debug.*
+
+  def run: IO[Unit] =
+    (ec("1"), ec("2")) match {
+      case (ec1, ec2) => for
+        _ <- IO("one").myDebug
+        // _ <- IO.shift(ec1) // CE2
+        _ <- IO("two").myDebug
+        // _ <- IO.shift(ec2)
+        _ <- IO("three").myDebug
+      yield ()
+    }
+
+  def ec(name: String): ExecutionContext =
+    ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor { r =>
+      val t = new Thread(r, s"pool-$name-thread-1")
+      t.setDaemon(true)
+      t
+    })
