1085ec8cde488e7ca05a0a69618aa7d0b934c36e
Bendeguz Seres <grafitpor@gmail.com>
2024-04-08 20:21:13 +0200

    Add stuff

---
+++ src/main/scala/scalaWithCats/Monads.scala
@@ -0,0 +1,49 @@
+package Monads
+
+// import cats.syntax.MonadOps
+// import cats.implicits._
+
+trait Monad[M[_]] {
+  def pure[A](a: A): M[A]
+  def bind[A,B](ma: M[A])(f: A => M[B]): M[B]
+}
+  
+object Monad {
+  def apply[M[_]](implicit instance: Monad[M]): Monad[M] = instance
+
+  implicit val OptionMonad: Monad[Option] = new Monad[Option] {
+    def pure[A](a: A) = Some(a)
+    def bind[A,B](ma: Option[A])(f: A => Option[B]): Option[B] = ma match
+      case Some(a) => f(a)
+      case None    => None
+  }
+
+  implicit val ListMonad: Monad[List] = new Monad[List] {
+    def join[A](ls: List[List[A]]): List[A] = ls.foldRight(Nil)((l: List[A], acc: List[A]) => l ++ acc)
+    def pure[A](a: A) = List(a)
+    def bind[A,B](as: List[A])(f: A => List[B]): List[B] = as match
+      case Nil => Nil
+      case as => join(as.map(f))
+  }
+  
+  // TODO Figure this out
+  // type EitherA[A] = Either[A,_]
+  // implicit def EitherMonad: Monad[EitherA] = new Monad[EitherA] {
+  //   def pure[A](a: A) = Right(a)
+  //   def bind[A,B](mab: EitherA[A])(f: B => Either[A,B]): Either[A,B] = mab match
+  //     case Left(a) => Left(a)
+  //     case Right(b) => f(b)
+  // }
+}
+
+// object MonadSyntax {
+//   implicit class MonadOps[M](value: M):
+//     def >>=[A,B](f: A => M[B])(implicit instance: Monad[M]) =
+//       instance.bind(value)(f)
+//
+// }
+
+
+@main
+def main: Unit =
+  println(Monad[List].bind(List(1,2,3))(List(_,2)))
