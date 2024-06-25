32f51d23de1a34ec7887d563d853379ebfc9b863
Bendeguz Seres <bendeguz.seres@emarsys.com>
2024-06-25 10:04:50 +0200

    Very important app

---
+++ src/main/scala/effects/Never.scala
@@ -0,0 +1,9 @@
+package effects
+
+import cats._
+import cats.effect._
+import scala.concurrent.duration._
+
+object NeverApp extends IOApp.Simple {
+  val run: IO[Unit] = (IO.println("lofasz") >> IO.sleep(2.second)).foreverM
+}
