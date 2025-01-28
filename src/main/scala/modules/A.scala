e9180cd7f15f97a2a192d632968a90515711a0e7
Bendeguz Seres <grafitpor@gmail.com>
2025-01-28 07:36:28 +0100

    Add stuff

--- src/main/scala/modules/A.scala
+++ src/main/scala/modules/A.scala
@@ -6,10 +6,16 @@ enum Value:
   private case FloatValue(value: Float)
   private case SpringValue(value: String)
 
+// Scala 2
+// trait Value
+// private case class BoolValue(value: Boolean) extends Value
+// private case class IntValue(value: Int) extends Value
+// private case class FloatValue(value: Float) extends Value
+// private case class SpringValue(value: String) extends Value
+
 object Value:
   def mkInt(value: Int): Option[Value] =
     if value < 0 || value > 100 then None else Some(IntValue(value))
-
 
 val x: Option[Value] = Value.mkInt(12)
 
